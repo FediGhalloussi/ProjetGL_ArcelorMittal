670f033208fd4f2b5f704d83dbd3a3c01d3e4fcc
Fedi Ghalloussi <fedi.ghalloussi@gmail.com>
2023-04-14 14:11:50 +0200

    add parameter

--- src/main/java/com/example/projetgl_ihm/Database/LoginUtil.java
+++ src/main/java/com/example/projetgl_ihm/Database/LoginUtil.java
@@ -5,6 +5,8 @@ import com.example.projetgl_ihm.Models.Engineer;
 import com.example.projetgl_ihm.Models.Worker;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginUtil {
 
@@ -52,4 +54,111 @@ public class LoginUtil {
         }
 
         return null;
-    }}
+    }
+
+    public static List<Employee> getAllUsers() {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        List<Employee> users = new ArrayList<>();
+
+        try {
+            Class.forName("org.h2.Driver");
+
+            // Replace with your own database connection details
+            String url = "jdbc:h2:tcp://localhost/~/test";
+            String user = "projetGL";
+            String password_db = "afa";
+            conn = DriverManager.getConnection(url, user, password_db);
+
+            stmt = conn.prepareStatement("SELECT USERNAME, PASSWORD,GRADE FROM USERS");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String grade = rs.getString("grade");
+
+                if (grade.equals("Worker")) {
+                    users.add(new Worker(username, password));
+                } else if (grade.equals("Engineer")) {
+                    users.add(new Engineer(username, password));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) rs.close();
+                if (stmt != null) stmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return users;
+    }
+
+    public static boolean registerUser(String username, String password, String grade) {
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            // Establish a connection to the database
+            Class.forName("org.h2.Driver");
+
+            // Replace with your own database connection details
+            String url = "jdbc:h2:tcp://localhost/~/test";
+            String user = "projetGL";
+            String password_db = "afa";
+            conn = DriverManager.getConnection(url, user, password_db);
+
+            // Check if the user already exists in the database
+            statement = conn.prepareStatement("SELECT * FROM Users WHERE Username = ?");
+            statement.setString(1, username);
+            resultSet = statement.executeQuery();
+
+            if (!resultSet.next()) {
+                // User does not exist, insert new user into database
+                statement = conn.prepareStatement("INSERT INTO Users (Username, Password, Grade) VALUES (?, ?, ?)");
+                statement.setString(1, username);
+                statement.setString(2, password);
+                statement.setString(3, grade);
+
+                statement.executeUpdate();
+
+                System.out.println("New user created!");
+                return true;
+            } else {
+                // User already exists
+                System.out.println("Username already exists, please choose a different username.");
+                return false;
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                // Close the database resources
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+
+                if (statement != null) {
+                    statement.close();
+                }
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+    }
+}
--- src/main/java/com/example/projetgl_ihm/GUI/ParametresController.java
+++ src/main/java/com/example/projetgl_ihm/GUI/ParametresController.java
@@ -1,24 +1,120 @@
 package com.example.projetgl_ihm.GUI;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.example.projetgl_ihm.GUI.DashboardOuvrierController;
+import com.example.projetgl_ihm.Models.Employee;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Node;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
+import static com.example.projetgl_ihm.Database.LoginUtil.*;
 
 public class ParametresController {
+
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private ListView<String> userList;
+
+    @FXML
+    private TextField userField;
+
+    @FXML
+    private ListView<String> userRightsList;
+
+    @FXML
+    private Button standButton;
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField inputRangeField;
+
+    @FXML
+    void handleAddUser() {
+        // Code to add user
+
+    }
+
+    @FXML
+    void handleSearchUser() {
+        // Code to search for user
+    }
+
+    @FXML
+    void handleAddRight() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO, new KeyValue(addButton.scaleXProperty(), 1.0), new KeyValue(addButton.scaleYProperty(), 1.0)),
+                new KeyFrame(Duration.millis(100), new KeyValue(addButton.scaleXProperty(), 1.2), new KeyValue(addButton.scaleYProperty(), 1.2)),
+                new KeyFrame(Duration.millis(200), new KeyValue(addButton.scaleXProperty(), 0.8), new KeyValue(addButton.scaleYProperty(), 0.8)),
+                new KeyFrame(Duration.millis(300), new KeyValue(addButton.scaleXProperty(), 1.1), new KeyValue(addButton.scaleYProperty(), 1.1)),
+                new KeyFrame(Duration.millis(400), new KeyValue(addButton.scaleXProperty(), 0.9), new KeyValue(addButton.scaleYProperty(), 0.9)),
+                new KeyFrame(Duration.millis(500), new KeyValue(addButton.scaleXProperty(), 1.0), new KeyValue(addButton.scaleYProperty(), 1.0))
+        );
+        timeline.play();
+        // Code to add user right
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        registerUser(username, password,"worker");
+    }
+
+    @FXML
+    void handleRemoveRight() {
+        // Code to remove user right
+    }
+
+    @FXML
+    void handleToggleStand() {
+        // Code to enable/disable stand
+    }
+
+    @FXML
+    void handleSaveInputRange() {
+        // Code to save level 2 input range
+    }
+
+
     @FXML
     private Button retourButton;
 
     @FXML
     public void initialize() {
         retourButton.setOnAction(this::retourAction);
+        // Create the ListView and the ObservableList to hold the items
+        ObservableList<String> userList = FXCollections.observableArrayList();
+
+        // Retrieve all the employees from the database
+        List<Employee> employees = getAllUsers();
+
+        // Add the usernames of the employees to the userList
+        for (Employee employee : employees) {
+            userList.add(employee.getUsername() + " : " + employee.getGrade());
+        }
+
+        // Set the items of the ListView to the userList
+        userRightsList.setItems(userList);
     }
 
 
@@ -42,5 +138,6 @@ public class ParametresController {
             e.printStackTrace();
         }
     }
+
 
 }
